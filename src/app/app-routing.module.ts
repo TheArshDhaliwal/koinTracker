e034507080bfa7845e24f81c2d6f0304297dbd0c
Arshdeep Singh <79arshdhaliwal@gmail.com>
2023-12-18 08:29:10 -0500

    Routing setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from './content/content.component';
+import { CoinPageComponent } from './coin-page/coin-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'content', pathMatch:'full'},
+  {path:'', component:ContentComponent},
+  {path:'coin-page', component:CoinPageComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
